90f4126a2b59139414adf16bb6e443a84e10d0e9
dineshsrimal <dineshsrimal@gmail.com>
2021-02-16 08:15:30 +0800

    CR1 1st change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("CR2");
-        System.out.println("CR1");
+
+        System.out.println("CR1 change");
     }
 }
